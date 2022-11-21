5972293e4a28e8455704c18dedd72cad417804a3
minicorco <minicorco@gmail.com>
2022-11-21 09:31:12 +0100

    Completed module

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -11,7 +11,6 @@ def load_directory(directory):
     for path in directory:
         load_module(directory.as_posix(path.stem))
 
-def load_bundled:
+def load_bundled():
     directory=Path(__file__).parent / "extensions"
     load_directory(directory)
-    
